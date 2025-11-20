21decdadd44ab6bf321e24afbd6cb03c42316fb7
HW-Howdy <84364883+HW-Howdy@users.noreply.github.com>
2025-11-20 15:58:00 +0900

    Update ResolutionUI.cs

--- Assets/Scripts/UI/Title/ResolutionUI.cs
+++ Assets/Scripts/UI/Title/ResolutionUI.cs
@@ -17,7 +17,7 @@ public class ResolutionUI : MonoBehaviour
 		int currentIndex = 0;
 
 		resolutions = Screen.resolutions;
-		_resolutionDropdown.ClearOptions();
+		_resolutionDropdown.ClearOptions();1
 		for (int i = 0; i < resolutions.Length; i++)
 		{
 			string option = $"{resolutions[i].width} X {resolutions[i].height}";
